b8b66d34771fcef6759b9ba0922685d27562f48b
unknown <houss@aliyun.com>
2018-09-13 20:21:27 +0800

    home-commit-9-14-gitignore

--- application/index/controller/Verify.php
+++ application/index/controller/Verify.php
@@ -16,7 +16,7 @@ class Verify extends IndexController
     ];
 
     public function get(){
-        
+
         $verifyModel = new VerifyModel();
         $logData = $this->requestData;
         unset($logData['captcha']);
