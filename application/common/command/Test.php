395c56c5c07c6b0ba99d8da93f7878af24fe70f3
unknown <houss@aliyun.com>
2018-09-17 22:53:34 +0800

    home-lock-order

--- application/common/command/Test.php
+++ application/common/command/Test.php
@@ -31,79 +31,16 @@ class Test extends Command
         $incomeOrders = Db::name('lock_order')->field('id,user_id,income')
             ->where('status=0')
             ->select();
-
-        //获取收益
-        //$sys = Db::name('config')->field('content')->where('id in(17,18,19,20)')->order('id asc')->select();
+        /*$incomeOrders = Db::name('lock_order')->alias('l')->field('l.id,l.user_id,l.income,l.money,p.rate')
+            ->join('p_lock_plan p','p.plan_id = l.plan_id')
+            ->where('l.status=0')
+            ->select();*/
 
         //遍历订单,计算用户理财收益
         foreach($incomeOrders as $v){
-            /*//获取用户父级树
-            $parentIds = Db::name('user')->where('user_id='.$v['user_id'])->value('parent_ids');
-            //判断父级树是否为空
-            if ($parentIds!= '') {
-                //获得父级树数组
-                $parentIds = explode('|', substr($parentIds, 1, count($parentIds)-2));
-                if(isset($parentIds[0])) {
-                     // bcmul    将两个高精度数字相乘
-                     // bcdiv    将两个高精度数字相除
-                     // $shareIncome1    一级父类收益
-                    $shareIncome1 = bcmul($v['income'],bcdiv($sys[0]['content'],100,4),4);
 
-                     // bccomp    比较两个高精度数字，返回-1,0,1
-
-                    if(bccomp($shareIncome1, 0, 4)>0) {
-                        Db::name('user')->where('user_id=' . $parentIds[0])->update([
-                            'share_income' => ['exp', 'share_income+' .$shareIncome1],
-                            'to_share_income' => ['exp', 'to_share_income+' . $shareIncome1],
-                            'ky_money'=>['exp', 'ky_money+'.$shareIncome1],
-                        ]);
-                        Db::name('money_log')
-                            ->insert(['user_id'=> $parentIds[0],'order_id'=>$v['id'], 'money'=>$shareIncome1, 'sign'=>'+', 'remark'=>'好友收益', 'type'=>6]);
-                    }
-                    if(isset($parentIds[1])) {
-                        $shareIncome2 = bcmul($v['income'],bcdiv($sys[1]['content'],100,4),4);
-                        if(bccomp($shareIncome2, 0, 4)>0) {
-                            Db::name('user')->where('user_id=' . $parentIds[1])->update([
-                                'share_income' => ['exp', 'share_income+' .$shareIncome2],
-                                'to_share_income' => ['exp', 'to_share_income+' . $shareIncome2],
-                                'ky_money'=>['exp', 'ky_money+'.$shareIncome2],
-                            ]);
-                            Db::name('money_log')
-                                ->insert(['user_id'=> $parentIds[1], 'order_id'=>$v['id'], 'money'=>$shareIncome2, 'sign'=>'+', 'remark'=>'好友收益', 'type'=>6]);
-                        }
-                        if(isset($parentIds[2])) {
-                            $shareIncome3 = bcmul($v['income'],bcdiv($sys[2]['content'],100,4),4);
-                            if(bccomp($shareIncome3, 0, 4)>0) {
-                                Db::name('user')->where('user_id=' . $parentIds[2])->update([
-                                    'share_income' => ['exp', 'share_income+' .$shareIncome3],
-                                    'to_share_income' => ['exp', 'to_share_income+' . $shareIncome3],
-                                    'ky_money'=>['exp', 'ky_money+'.$shareIncome3],
-                                ]);
-                                Db::name('money_log')
-                                    ->insert(['user_id'=> $parentIds[2], 'order_id'=>$v['id'], 'money'=>$shareIncome3, 'sign'=>'+', 'remark'=>'好友收益', 'type'=>6]);
-                            }
-                            if(count($parentIds) > 3) {
-                                unset($parentIds[0], $parentIds[1], $parentIds[2]);
-                                $shareIncome4 = bcmul($v['income'],bcdiv($sys[3]['content'],100,4),4);
-                                if(bccomp($shareIncome4, 0, 4)>0) {
-                                    Db::name('user')->where('user_id in(' . implode(',',$parentIds).')')->update([
-                                        'share_income' => ['exp', 'share_income+' .$shareIncome4],
-                                        'to_share_income' => ['exp', 'to_share_income+' . $shareIncome4],
-                                        'ky_money'=>['exp', 'ky_money+'.$shareIncome4],
-                                    ]);
-                                    $insertAll = [];
-                                    foreach($parentIds as $id){
-                                      $insertAll[] = ['user_id'=> $id,'order_id'=>$v['id'], 'money'=>$shareIncome4, 'sign'=>'+', 'remark'=>'好友收益', 'type'=>6];
-                                    }
-                                    Db::name('money_log')
-                                        ->insertAll($insertAll);
-                                }
-                            }
-                        }
-                    }
-                }
-            }*/
-
+            //理财收益
+            //$lock_profit =bcmul(bcdiv($v['rate'],100,4),$v['money'],4);
             Db::name('user')->where('user_id='.$v['user_id'])
                 ->update([
                     'ky_money'=>['exp', 'ky_money+'.$v['income']],
