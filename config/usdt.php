224e04abdaea51666bff24f5ee5f4c467726bbf4
unknown <houss@aliyun.com>
2018-09-18 20:57:02 +0800

    home-test

--- config/usdt.php
+++ config/usdt.php
@@ -7,7 +7,7 @@ return [
         // 数据库类型
         'type'            => 'mysql',
         // 服务器地址
-        'hostname'        => '93.90.74.21',
+        'hostname'        => 'localhost',
         // 数据库名
         'database'        => 'ltc',
         // 用户名
